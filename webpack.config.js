5fc9d18c8d9330c72b447534cf703f2a15a87136
Zombiefruit <kieranw@wix.com>
2020-03-30 17:06:04 +0300

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const { StylableWebpackPlugin } = require('@stylable/webpack-plugin');
+require('@ts-tools/node/r');
+
+const plugins = [new StylableWebpackPlugin()];
+
+module.exports = {
+    // root of the monorepo, so that paths in output will be clickable
+    context: __dirname,
+
+    // works great. with the default 'eval', imports are not mapped.
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /\.d\.ts$/,
+                loader: '@ts-tools/webpack-loader',
+                options: {
+                    configFilePath: require.resolve('./tsconfig.build.json')
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /\.st\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg|woff2|ttf)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 2048
+                }
+            }
+        ],
+        noParse: [require.resolve('typescript/lib/typescript.js')]
+    },
+    plugins
+};
