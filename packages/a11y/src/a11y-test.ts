6801665ab358bc9ff1f763e4b39c978bb6c8c998
Zombiefruit <kieranw@wix.com>
2020-03-30 16:30:25 +0300

    Add comment

--- packages/a11y/src/a11y-test.ts
+++ packages/a11y/src/a11y-test.ts
@@ -1,6 +1,9 @@
 import { ISimulation, renderSimulation } from '@wixc3/wcs-core';
 import axe, { run } from 'axe-core';
 
+/*
+ * Must be run in the browser, as axe-core depends on it.
+ */
 export const checkIfSimulationIsAccessible = async (
     simulation: ISimulation<Record<string, unknown>>
 ): Promise<axe.AxeResults> => {
