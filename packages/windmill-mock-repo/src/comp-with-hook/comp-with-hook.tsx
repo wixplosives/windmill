3733a7f5ea2952cdde0760de639fbc3ce48fa7f8
Avi Vahl <avi.vahl@wix.com>
2020-05-25 18:34:14 +0300

    Correct lint error post merge

--- packages/windmill-mock-repo/src/comp-with-hook/comp-with-hook.tsx
+++ packages/windmill-mock-repo/src/comp-with-hook/comp-with-hook.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useCallback } from 'react';
 
+const addOne = (value: number) => value + 1;
+
 export const CompWithHook: React.FC = () => {
     const [state, setState] = useState<number>(1);
+    const increment = useCallback(() => setState(addOne), []);
 
-    function increment() {
-        setState(state + 1);
-    }
-
-    const wrappedIncrement = useCallback(increment, []);
-
-    return <div onClick={wrappedIncrement}>{state}</div>;
+    return <div onClick={increment}>{state}</div>;
 };
