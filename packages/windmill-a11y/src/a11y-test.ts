ca37ee4036475b7294121475bcf7ab1f6ef2155b
Avi Vahl <avi.vahl@wix.com>
2020-08-17 22:00:40 +0300

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- packages/windmill-a11y/src/a11y-test.ts
+++ packages/windmill-a11y/src/a11y-test.ts
@@ -5,9 +5,7 @@ import type { IA11yTestResult } from '@wixc3/windmill-utils/src';
 /*
  * Must be run in the browser, as axe-core depends on it.
  */
-export const checkIfSimulationIsAccessible = async (
-    simulation: ISimulation<Record<string, unknown>>
-): Promise<IA11yTestResult> => {
+export const checkIfSimulationIsAccessible = async (simulation: ISimulation<any>): Promise<IA11yTestResult> => {
     const { canvas, cleanup } = renderSimulation(simulation);
 
     const results = await run(canvas);
