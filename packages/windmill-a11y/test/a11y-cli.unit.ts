978668ec38eaa4100d6f7b189432e05f85681279
Zombiefruit <kieranw@wix.com>
2020-09-06 13:59:49 +0300

    File services maybe?

--- packages/windmill-a11y/test/a11y-cli.unit.ts
+++ packages/windmill-a11y/test/a11y-cli.unit.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
-import { dirname, resolve, join } from 'path';
+import { dirname, resolve } from 'path';
 import { spawnSync } from 'child_process';
+import { join } from '@file-services/path';
 
 const cliSrcPath = require.resolve('../bin/windmill-a11y.js');
 const mockRepoRoot = dirname(require.resolve('@wixc3/windmill-mock-repo/package.json'));
